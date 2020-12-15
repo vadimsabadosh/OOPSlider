0c945c119ee53b8b2173e0686e1c13602c958284
Vadim Sabadosh <vadimsabadosh@gmail.com>
2020-12-15 13:42:28 +0200

    Задание урока 19 - Выполнено

--- script/script.js
+++ script/script.js
@@ -22,6 +22,7 @@ window.addEventListener('DOMContentLoaded', function () {
         function updateClock() {  
 
             let timer = getTimeRemaining();
+            
             const addZero = (n) => {
                 return n < 10 ? '0' + n: n;
             }
@@ -41,4 +42,84 @@ window.addEventListener('DOMContentLoaded', function () {
     }
     countTimer('16 december 2020');
     
+
+    //Menu 
+    const toggleMenu = () => {
+        const btnMenu = document.querySelector('.menu');
+        const btnClose = document.querySelector('.close-btn');
+        const menu = document.querySelector('menu');
+        const menuItems = menu.querySelectorAll('ul>li');
+
+        const handlerMenu = () => {
+            menu.classList.toggle('active-menu');        
+        };
+
+        btnMenu.addEventListener('click', handlerMenu);
+        btnClose.addEventListener('click', handlerMenu);
+        menuItems.forEach(item => item.addEventListener('click', handlerMenu));
+
+    };
+    toggleMenu();
+
+
+
+    //POP-UP
+
+    const togglePopup = () => {
+        const popup = document.querySelector('.popup');
+        const btnPopup = document.querySelectorAll('.popup-btn');
+        const popupClose = popup.querySelector('.popup-close');
+
+        //Функции Анимации
+
+        //Функция плавного исчезновения
+        function fadeOut(el) {
+            el.style.opacity = 1;
+            function fade() {
+                if ((el.style.opacity -= .05) < 0) {
+                    el.style.opacity = '';
+                    el.style.display = "none";
+                } else {
+                    requestAnimationFrame(fade);
+                }
+            };
+            fade();
+        };
+        //Фукнция плавного появления
+        function fadeIn(el, display) {
+            el.style.opacity = 0;
+            el.style.display = display || "block";
+            function fade() {
+                var val = parseFloat(el.style.opacity);
+                if (!((val += .05) > 1)) {
+                    el.style.opacity = val;
+                    requestAnimationFrame(fade);
+                }
+            };
+            fade();
+        };
+
+        //Обработчики событий
+        btnPopup.forEach(item => {
+            item.addEventListener('click', () =>{
+
+                if(document.documentElement.clientWidth>768){
+                    fadeIn(popup);
+                }else{
+                    popup.style.display = 'block';
+                }
+            });
+        })
+        popupClose.addEventListener('click', () => {
+
+            if(document.documentElement.clientWidth>768){
+                fadeOut(popup)
+            }else{
+                popup.style.display = '';
+            }
+        
+        })
+
+    }
+    togglePopup();
 });
